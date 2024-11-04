02ee5aae4561f682fcd3cad16674ce93f4b441d5
alexander <molokan2007@mail.ru>
2024-11-04 21:25:38 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+
 import time
 from time import sleep
 import threading
@@ -11,7 +12,7 @@ def write_words(word_count , file_name):
             file.write(word + '\n')
             i+=i
             sleep(0.1)
-        print(f'Завершилась запись в файл {file_name}.txt \n')
+        print(f'Завершилась запись в файл {file_name}.txt')
 
 
 start_time = time.time()
@@ -23,16 +24,7 @@ write_words(100, 'example4')
 
 end_time = time.time()
 el_time = (end_time - start_time)
-print(el_time.__round__(3))
-
-# После вызовов функций создайте 4 потока для вызова этой функции со следующими аргументами для функции:
-# 10, example5.txt
-# 30, example6.txt
-# 200, example7.txt
-# 100, example8.txt
-# Запустите эти потоки методом start не забыв, сделать остановку основного потока при помощи join.
-# Также измерьте время затраченное на выполнение функций и потоков.
-# Как это сделать рассказано в лекции к домашнему заданию.
+print(f'Врамя потоков последовательно: {el_time.__round__(4)} c')
 
 thread_1 = threading.Thread(target= write_words, args= (10 , 'example5'))
 thread_2 = threading.Thread(target= write_words, args= (30 , 'example6'))
@@ -44,6 +36,10 @@ thread_1.start()
 thread_2.start()
 thread_3.start()
 thread_4.start()
+thread_1.join()
+thread_2.join()
+thread_3.join()
+thread_4.join()
 end_time_THR = time.time()
 el_time_THR = (end_time_THR - start_time_THR)
-print(f'Время потоков:  {el_time_THR.__round__(4)}')
+print(f'Время потоков параллельно:  {el_time_THR.__round__(4)} c')
